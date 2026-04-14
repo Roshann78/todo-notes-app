d7c1b7e415b46c7e25f0373c539a1182f62b47c8
Roshann78 <grd.roshan78@gmail.com>
2026-04-14 22:16:15 +0530

    feat: install framer-motion and react-hot-toast, add Toaster to main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 import { AuthProvider } from './context/AuthContext'
+import { Toaster } from 'react-hot-toast'
 import './index.css'
 import App from './App.jsx'
 
@@ -9,6 +10,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
       <AuthProvider>
+        <Toaster />
         <App />
       </AuthProvider>
     </GoogleOAuthProvider>
